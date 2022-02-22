14a4c9d84acd95dbad8beda6be0d52e9c3072681
Wenjing <q564756917@gmail.com>
2022-02-21 22:00:21 -0600

    Finished

--- issoverhead-start/main.py
+++ issoverhead-start/main.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
@@ -11,8 +13,7 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
+# Your position is within +5 or -5 degrees of the ISS position.
 
 parameters = {
     "lat": MY_LAT,
@@ -28,10 +29,25 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+
 # BONUS: run the code every 60 seconds.
+while True:
+    time.sleep(60)
+    # If the ISS is close to my current position
+    if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+        # and it is currently dark
+        if time_now.hour > sunset or time_now.hour < sunrise:
+            # Then send me an email to tell me to look up.
+            with smtplib.SMTP('smtp.gmail.com') as connection:
+                connection.starttls()
+                connection.login(user='test0827111.gmail.com',password='Nihaoya123!')
+                connection.sendmail(from_addr='test0827111.gmail.com',
+                                    to_addrs='q564756917@yahoo.com',
+                                    msg='Subject: Look up!\n\nHey, look up now~')
+
+
+
+
 
 
 
